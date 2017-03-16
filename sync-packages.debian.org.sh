6831e0113350a7d43066cb268c976aadd516f273
anatoly techtonik <techtonik@gmail.com>
2017-03-16 13:56:18 +0300

    Create sync-packages.debian.org.sh
    Sync packages.debian.org (#6)

---
+++ sync-packages.debian.org.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PROJECT=packages.debian.org
+SOURCE=git://anonscm.debian.org/webwml/packages.git
+TARGET=git@github.com:rirror/packages.debian.org.git
+
+git clone --mirror $SOURCE $PROJECT
+git -C $PROJECT push --mirror $TARGET
