2c0fe156ff95775ec4d768a38d0cc2b726a7ae16
anatoly techtonik <techtonik@gmail.com>
2017-05-22 04:02:30 +0300

    Skip git clone if directory exists

--- sync-packages.debian.org.sh
+++ sync-packages.debian.org.sh
@@ -3,6 +3,8 @@ PROJECT=packages.debian.org
 SOURCE=git://anonscm.debian.org/webwml/packages.git
 TARGET=git@github.com:rirror/packages.debian.org.git
 
-git clone --mirror $SOURCE $PROJECT
+# skip git clone if directory exists
+[[ -d $PROJECT ]] || git clone --mirror $SOURCE $PROJECT
+
 git -C $PROJECT fetch -p
 git -C $PROJECT push --mirror $TARGET
