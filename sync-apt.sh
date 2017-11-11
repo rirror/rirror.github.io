e058c31ccb91bcb30c59dc221a23f55d618a363d
anatoly techtonik <techtonik@gmail.com>
2017-11-11 12:02:47 +0300

    Create sync-apt.sh

---
+++ sync-apt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Edit these variables for your project
+PROJECT=apt
+SOURCE=git://anonscm.debian.org/git/apt/apt.git
+TARGET=git@github.com:rirror/apt.git
+
+# make sure deploy key is added to target repository
+export GIT_SSH_COMMAND="ssh -i /root/.ssh/$PROJECT"
+
+# skip git clone if directory exists
+[[ -d $PROJECT ]] || git clone --mirror $SOURCE $PROJECT
+
+git -C $PROJECT fetch -p
+git -C $PROJECT push --mirror $TARGET
