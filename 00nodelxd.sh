e356f75e9df67ee89db469a1e651f89eb1b5bd3b
anatoly techtonik <techtonik@gmail.com>
2019-01-09 15:58:33 +0300

    Quote echo params to avoid expansion surprises
    Like this one:
    
        ./00nodelxd.sh: 12: ./00nodelxd.sh: Syntax error: "(" unexpected

--- 00nodelxd.sh
+++ 00nodelxd.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-echo --- creating rirror node ---
+echo "--- creating rirror node ---"
 lxc launch ubuntu:18.10 rirror
 
-echo --- installing updates ---
+echo "--- installing updates ---"
 lxc exec rirror -- apt-get -y update
 # unattended-upgrades is preinstalled in 18.10
 #lxc exec rirror -- apt-get -y install unattended-upgrades
 lxc exec rirror -- unattended-upgrades -v
 
-echo --- installing git, ipython (bash replacement) and jobber ---
+echo "--- installing git, ipython (bash replacement) and jobber ---"
 lxc exec rirror -- apt-get -y install git ipython
 lxc exec rirror -- curl -LO https://github.com/dshearer/jobber/releases/download/v1.2.1/jobber_1.2.1-1_amd64.deb
 lxc exec rirror -- dpkg -i jobber_1.2.1-1_amd64.deb 
 
-echo --- adding github.com to known_hosts ---
+echo "--- adding github.com to known_hosts ---"
 lxc exec rirror -- bash -c "ssh-keyscan github.com >> ~/.ssh/known_hosts"
 
-echo --- uploading sync scripts ---
+echo "--- uploading sync scripts ---"
 lxc file push sync-*.sh rirror/root/
 lxc exec rirror -- bash -c "chmod +rx *.sh"
 
-echo --- generating deploy keys ---
+echo "--- generating deploy keys ---"
 # keys are named after GitHub repositories
 # e.g. /root/.ssh/bugs.debian.org
 # and should be pasted to project settings manually
@@ -29,7 +29,7 @@ lxc file push 01genkeys.ipy rirror/root/
 lxc exec rirror -- chmod +rx 01genkeys.ipy
 lxc exec rirror -- ./01genkeys.ipy
 
-echo --- setting up daily jobber ---
+echo "--- setting up daily jobber ---"
 # set uid to 0 (root) to allow jobber load the file
 lxc file push .jobber rirror/root/ --uid=0
 lxc exec rirror -- jobber reload
