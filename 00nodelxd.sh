14a3ae130aa994fc42e8de1e6b2c94dd0c2013b8
anatoly techtonik <techtonik@gmail.com>
2019-01-09 15:52:55 +0300

    Up LXD node bootstrap script for Ubuntu 18.10
    * unattended-upgrades are preinstalled
    * ipython is used as bash alternative
    * jobber 1.2.1 with more secure go

--- 00nodelxd.sh
+++ 00nodelxd.sh
@@ -1,15 +1,18 @@
+#!/bin/sh
+
 echo --- creating rirror node ---
-lxc launch ubuntu:z/amd64 rirror
+lxc launch ubuntu:18.10 rirror
 
 echo --- installing updates ---
 lxc exec rirror -- apt-get -y update
-lxc exec rirror -- apt-get -y install unattended-upgrades
+# unattended-upgrades is preinstalled in 18.10
+#lxc exec rirror -- apt-get -y install unattended-upgrades
 lxc exec rirror -- unattended-upgrades -v
 
-echo --- installing git and jobber ---
+echo --- installing git, ipython (bash replacement) and jobber ---
 lxc exec rirror -- apt-get -y install git ipython
-lxc exec rirror -- curl -LO https://github.com/dshearer/jobber/releases/download/v1.2/jobber_1.2-1_amd64.deb
-lxc exec rirror -- dpkg -i jobber_1.2-1_amd64.deb 
+lxc exec rirror -- curl -LO https://github.com/dshearer/jobber/releases/download/v1.2.1/jobber_1.2.1-1_amd64.deb
+lxc exec rirror -- dpkg -i jobber_1.2.1-1_amd64.deb 
 
 echo --- adding github.com to known_hosts ---
 lxc exec rirror -- bash -c "ssh-keyscan github.com >> ~/.ssh/known_hosts"
